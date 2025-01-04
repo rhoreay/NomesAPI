b34240df6d1ce3349ceb26aff94ff3dadbecde0d
marcio <hugofelicio01@gmail.com>
2025-01-04 14:08:05 +0000

    Add support for CORS using Flask-Cors

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 from flask import Flask
+from flask_cors import CORS
 from buscaNome import pegaDadosNome
-app = Flask(__name__)
 
+app = Flask(__name__)
+CORS(app)  # Habilita CORS para todas as rotas
 
 @app.route('/api/<string:nome>/')
 def buscaNome(nome):
     response = pegaDadosNome(nome)
-    if response == None:
+    if response is None:
         return ['nome não encontrado']
     else:
         return [nome, response]
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=602)
-
